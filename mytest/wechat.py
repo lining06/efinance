a9d27cd7b9eacd3f9bdd502596098e381fbb0621
lining <lining@soulapp.cn>
2023-02-08 14:16:15 +0800

    test

---
+++ mytest/wechat.py
@@ -0,0 +1,14 @@
+import json
+
+import requests
+msg = 'Hello World'
+data = {
+            "msgtype": "text",
+            "text": {
+                "content": msg,
+                "mentioned_mobile_list": ["18516288649"]
+            }
+        }
+r = requests.post(url='https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=80367921-d43c-4b87-bb6a-1628b9bb7e6f',
+                  data=json.dumps(data))
+r
