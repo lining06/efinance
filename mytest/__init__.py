55779159f7b3a48abafc9d9c27641c32feb44342
lining <lining@soulapp.cn>
2023-02-08 14:26:31 +0800

    test

--- mytest/__init__.py
+++ mytest/__init__.py
@@ -5,6 +5,7 @@ import efinance as ef
 import requests
 
 import jsonpath
+
 # 股票代码
 stock_code = '600519'
 # 开始日期
@@ -13,17 +14,5 @@ beg = '20210101'
 end = '20210708'
 # 获取股票日 K 数据
 df = ef.stock.get_quote_history(stock_code, beg=beg, end=end)
-
-
-data = {
-            "msgtype": "text",
-            "text": {
-                "content": df,
-                "mentioned_mobile_list": ["18516288649"]
-            }
-        }
-r = requests.post(url='https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=80367921-d43c-4b87-bb6a-1628b9bb7e6f',
-                  data=json.dumps(data))
-
 
 print(df)
