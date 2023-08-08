93b53e1dbeed56fc9dc257bf9f999fe1dab65d47
李宁 <lining@soulapp.cn>
2023-08-08 11:00:19 +0800

    增加索引

--- myShares/mysql_connect/sharesCodeMysql.py
+++ myShares/mysql_connect/sharesCodeMysql.py
@@ -1,13 +1,16 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    password="ln6@Asiain",
-    autocommit=True,
-    database="lining",
-    pool_size=10  # 设置连接池的大小为10
-)
+
+def get_connection():
+    mydb = mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="ln6@Asiain",
+        autocommit=True,
+        database="lining",
+        pool_size=10  # 设置连接池的大小为10
+    )
+    return mydb
 
 
 def saveSharesCode(shares_name,
@@ -28,6 +31,7 @@ def saveSharesCode(shares_name,
 def insertSharesDay(shares_name,
                     shares_code,
                     shares_type):
+    mydb = get_connection()
     mycursor = mydb.cursor()
     sql = "INSERT INTO shares_code (" \
           "shares_name, shares_code, shares_type) " \
@@ -38,9 +42,11 @@ def insertSharesDay(shares_name,
     print("insert success " + shares_name + " " + shares_code)
     mydb.close()
 
+
 def update(shares_name,
            shares_code,
            shares_type):
+    mydb = get_connection()
     mycursor = mydb.cursor()
     sql = "UPDATE shares_code SET shares_name=%s where shares_code=%s and shares_type=%s"
     val = (shares_name, shares_code, shares_type)
@@ -51,6 +57,7 @@ def update(shares_name,
 
 
 def select(shares_code, shares_name):
+    mydb = get_connection()
     mycursor = mydb.cursor()
     sql = "select shares_name, shares_code from shares_code where shares_code = %s and shares_name = %s"
     val = (shares_code, shares_name)
@@ -64,6 +71,7 @@ def select(shares_code, shares_name):
 
 
 def selectAll(offset, limit):
+    mydb = get_connection()
     mycursor = mydb.cursor()
     sql = "select shares_name, shares_code, shares_type from shares_code order by id asc limit %s,%s "
     val = (offset, limit)
@@ -77,6 +85,7 @@ def selectAll(offset, limit):
 
 
 def selectCount(shares_type):
+    mydb = get_connection()
     mycursor = mydb.cursor()
     sql = "select count(1) from shares_code where shares_type = %s "
     val = (shares_type,)
